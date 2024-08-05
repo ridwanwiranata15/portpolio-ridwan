92675bd84bd542bcc7d50d31d37de27f41fc48d8
ridwanwiranata15 <jagokoding15@gmail.com>
2024-08-05 09:01:39 +0700

    portpolio

---
+++ dist/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+     //hamburger
+const hamburger = document.querySelector("#hamburger")
+const navMenu = document.querySelector("#nav-menu")
+
+
+hamburger.addEventListener("click", function(e){
+    e.preventDefault();
+    hamburger.classList.toggle("hamburger-active")
+    navMenu.classList.toggle("hidden");
+})
+
+window.onscroll = function(){
+   const header = document.querySelector("header")
+   const fixedNav = header.offsetTop;
+
+   if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed')
+   }else{
+        header.classList.remove('navbar-fixed')
+   }
+}
+})
